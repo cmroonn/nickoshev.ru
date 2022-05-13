aadf673322c533efc3a077fcb1567cbad604982d
cmroonn <fallgetell@gmail.com>
2022-05-13 17:43:33 +0500

    init

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  {
+    var loader = document.getElementById('loading');
+    setTimeout(function () {
+      return loader.classList.add('hide');
+    }, 2000);
+  }
+});
