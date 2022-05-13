00498f39ebe9774dea472c3128891d8e991693cd
cmroonn <fallgetell@gmail.com>
2022-05-13 17:27:36 +0500

    init

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    {
+        const loader = document.getElementById('loading');
+        setTimeout(() => loader.classList.add('hide'), 2000);
+
+    }
+});
